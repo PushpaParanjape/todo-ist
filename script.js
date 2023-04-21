7abc02172441c606bea6f38001c5bd38736f03d1
PushpaParanjape <pushapaparanjape28@gmail.com>
2023-04-21 12:55:33 +0530

    latest

--- script.js
+++ script.js
@@ -6,9 +6,42 @@ function addTask() {
     if (inputBox.value === '') {
         alert("Please Write something!");
     } else {
+        // The entered element should display in list container(html)
         let li = document.createElement("li");
         li.innerHTML = inputBox.value;
         listContainer.appendChild(li);
-        // The entered element should display in list container(html)
+
+        // creating cross icon X for removing the particular task from the list 
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
     }
+    inputBox.value = ""; //For clear the input field after click on add button
+    saveData(); //whenever we will add task this fun will be called and save data in your browser
+
 }
+
+//for click function(checked unchecked and remove)
+listContainer.addEventListener("click", function(e) {
+    if (e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+        saveData(); //whenever we will add task this fun will be called and save data in your browser
+
+
+    } else if (e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+        saveData(); //whenever we will add task this fun will be called and save data in your browser
+
+    }
+}, false);
+
+//for display your tasks as it is when you close your browser(save data)
+function saveData() {
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+//Display Data
+function shoeTask() {
+    listContainer.innerHTML = localStorage.getItem("data");
+
+}
+shoeTask();
